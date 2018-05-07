bdb9a0eae53be7b314d4465dbde0ec7747bd5e4e
Zach Behnke <ztbehnke62@gmail.com>
2018-05-06 21:59:49 -0700

    Named regex group. Tweaked some patterns.

--- app/services/parser_service.py
+++ app/services/parser_service.py
@@ -1,16 +1,18 @@
 import re
 
 class ParserService:
+    LOCATION_GROUP_NAME = 'location'
+
     def __init__(self):
-        self.weather_patterns = ["what's the weather in (.*)",
-                                 "weather in (.*)",
-                                 "(.*) weather$"]
+        self.weather_patterns = [f"what('s| is) the weather in (?P<{self.LOCATION_GROUP_NAME}>.*)",
+                                 f"weather (in|at) (?P<{self.LOCATION_GROUP_NAME}>.*)",
+                                 f"(?P<{self.LOCATION_GROUP_NAME}>.*) weather$"]
 
     def find_location_for_weather(self, message):
         formatted_message = message.lower().rstrip('?:!.,;')
         for pattern in self.weather_patterns:
             match = re.match(pattern, formatted_message)
-            if (match and match.group(1)):
-                return match.group(1)
+            if (match and match.group(self.LOCATION_GROUP_NAME)):
+                return match.group(self.LOCATION_GROUP_NAME)
 
         return None
